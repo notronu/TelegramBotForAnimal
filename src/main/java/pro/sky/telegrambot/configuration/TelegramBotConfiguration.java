39db71f34e798c02b853d05d5e14ab14ed7748c7
Chika_Pyu <ya.sasha-mochalov1@yandex.ru>
2024-07-30 15:59:30 +0300

    Раскрытие функционала

--- src/main/java/pro/sky/telegrambot/configuration/TelegramBotConfiguration.java
+++ src/main/java/pro/sky/telegrambot/configuration/TelegramBotConfiguration.java
@@ -1,26 +1,35 @@
 package pro.sky.telegrambot.configuration;
 
-import com.pengrad.telegrambot.TelegramBot;
-import com.pengrad.telegrambot.model.DeleteMyCommands;
+import lombok.Data;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
+@Data
+@PropertySource("application.properties")
 public class TelegramBotConfiguration {
-
-
-
-
 
     @Value("${telegram.bot.token}")
     private String token;
 
-    @Bean
-    public TelegramBot telegramBot() {
-        TelegramBot bot = new TelegramBot(token);
-        bot.execute(new DeleteMyCommands());
-        return bot;
+    @Value("${telegram.bot.name}")
+    private String name;
+
+    @Value("{telegram.bot.volunteer}")
+    private String volunteer;
+
+//    @Bean
+//    public PetService petService(){
+//    return new PetService();
+//    }
+
+    public String getToken() {
+        return token;
     }
 
+    public String getName() {
+        return name;
+    }
 }
