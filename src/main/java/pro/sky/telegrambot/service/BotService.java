59dad09330b7c111d83836e8ff4658fc041229b8
Chika_Pyu <ya.sasha-mochalov1@yandex.ru>
2024-08-21 15:55:43 +0300

    Продолжение работы

--- src/main/java/pro/sky/telegrambot/service/BotService.java
+++ src/main/java/pro/sky/telegrambot/service/BotService.java
@@ -11,11 +11,14 @@ import com.pengrad.telegrambot.request.SendMessage;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import pro.sky.telegrambot.model.AnimalType;
 import pro.sky.telegrambot.repository.ShelterRepository;
+import pro.sky.telegrambot.repository.UserRepository;
 import pro.sky.telegrambot.util.LocationUtil;
+
+import java.util.HashMap;
 import java.util.Map;;
 import java.util.concurrent.ConcurrentHashMap;
-
-
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 
 /**
@@ -27,82 +30,146 @@ public class BotService {
     private final TelegramBot telegramBot;
     private final ShelterRepository shelterRepository;
     private final Keyboard mainMenuKeyboard;
+    private final Keyboard instructionMenuKeyboard;
     private static final Map<Long, Integer> incorrectCounts = new ConcurrentHashMap<>();
     private static final Logger logger = LogManager.getLogger(BotService.class);
+    private static final Pattern PHONE_PATTERN = Pattern.compile("\\+7-9\\d{2}-\\d{3}-\\d{2}-\\d{2}");
+    private static final Pattern ANSWER_PATTERN = Pattern.compile("([0-9\\.\\:\\s]{16})(\\s)([\\W+]+)");
+    private static final Pattern ANSWER_PATTERN1 = Pattern.compile("([\\W+]+)(\n)([\\W+]+)(\n)([\\W+]+)");
+    private static final Pattern ANSWER_PATTERN2 = Pattern.compile("[0-9]");
+    private UserService userService;
+    private final UserRepository repository;
+    private Map<Long, String> userStates = new HashMap<>();
 
 
-    public BotService(TelegramBot telegramBot, ShelterRepository shelterRepository) {
+    public BotService(TelegramBot telegramBot, ShelterRepository shelterRepository, UserRepository repository, UserService userService) {
+        this.userService = userService;
+        this.repository = repository;
         this.telegramBot = telegramBot;
         this.shelterRepository = shelterRepository;
         this.mainMenuKeyboard = new ReplyKeyboardMarkup(
                 new String[]{"Выбор приюта", "Инструкция как взять животное из приюта"},
                 new String[]{"Прислать отчет о питомце", "Позвать волонтера"},
-                new String[]{"Правила безопасности на территории приюта", "Информация о приюте "}
-
+                new String[]{"Информация о приюте "}
         );
+        this.instructionMenuKeyboard = new ReplyKeyboardMarkup(
+                new String[]{"Инструкция по знакомству с животным","Правила безопасности на территории приюта"},
+                new String[]{"Инструкция по обустройству дома", "Рекомендации по проверенным кинологам"},
+                new String[]{"Обустройство дома для питомца с ограниченными возможностями", "Информация по транспортировке питомца"},
+                new String[]{"Позвать волонтера", "Запросить связь", "Главное меню"});
     }
     public void handleUpdate(Update update) {
         if (update.message() != null && update.message().text() != null) {
             String text = update.message().text();
             long chatId = update.message().chat().id();
+            String login = update.message().from().lastName();
+            var state = userStates.get(chatId);
+            var user = userService.findByUser(chatId);
             boolean understood = false;
-            switch (text) {
-                case "Выбор приюта":
-                    sendShelterChoiceMenu(chatId);
-                    understood = true;
-                    break;
-                case "Информация о приюте":
-                    informationAboutShelter(chatId);
-                    understood = true;
-                    break;
-                case "Инструкция как взять животное из приюта":
-                    sendInstruction(chatId);
-                    understood = true;
-                    break;
-                case "Прислать отчет о питомце":
-                    sendPetReport(chatId);
-                    understood = true;
-                    break;
-                case "Позвать волонтера":
-                    callVolunteer(chatId, text);
-                    understood = true;
-                    break;
-                case "Информация о приюте для кошек":
-                    sendCatShelterInfo(chatId);
-                    understood = true;
-                    break;
-                case "Местоположение приюта для кошек":
-                    LocationUtil.sendCatShelterLocation(chatId, AnimalType.CAT);
-                    understood = true;
-                    break;
-                case "Правила безопасности на территории приюта":
-                    safetyEquipment(chatId, text);
-                    understood = true;
-                    break;
-                case "Информация о приюте для собак":
-                    sendDogShelterInfo(chatId);
-                    understood = true;
-                    break;
-                case "Местоположение приюта для собак":
-                    LocationUtil.sendDogShelterLocation(chatId, AnimalType.DOG);
-                    understood = true;
-                    break;
-                default:
-                    sendMainMenu(chatId);
-                    break;
-            }
 
-            if (!understood) {
-                var count = incorrectCounts.getOrDefault(chatId, 0);
-                if (count < 1) {
-                    incorrectCounts.put(chatId, count + 1);
-                    writeIncorrectText(chatId, text);
-                } else {
-                    writeIncorrectText2(chatId, text);
-                    incorrectCounts.remove(chatId);
+            if ("PhoneListener".equals(state)) {
+                handleContactInput(chatId, text);
+                userStates.remove(chatId);
+            } else {
+                switch (text) {
+                    case "Выбор приюта":
+                        sendShelterChoiceMenu(chatId);
+                        understood = true;
+                        break;
+                    case "Информация о приюте":
+                        informationAboutShelter(chatId);
+                        understood = true;
+                        break;
+                    case "Инструкция как взять животное из приюта":
+                        sendInstruction(chatId);
+                        understood = true;
+                        break;
+                    case "Прислать отчет о питомце":
+                        sendPetReport(chatId);
+                        understood = true;
+                        break;
+                    case "Позвать волонтера":
+                        callVolunteer(chatId, text);
+                        understood = true;
+                        break;
+                    case "Информация о приюте для кошек":
+                        sendCatShelterInfo(chatId);
+                        understood = true;
+                        break;
+                    case "Местоположение приюта для кошек":
+                        LocationUtil.sendCatShelterLocation(chatId, AnimalType.CAT);
+                        understood = true;
+                        break;
+                    case "Правила безопасности на территории приюта":
+                        safetyEquipment(chatId, text);
+                        understood = true;
+                        break;
+                    case "Информация о приюте для собак":
+                        sendDogShelterInfo(chatId);
+                        understood = true;
+                        break;
+                    case "Местоположение приюта для собак":
+                        LocationUtil.sendDogShelterLocation(chatId, AnimalType.DOG);
+                        understood = true;
+                        break;
+                    case "Информация по транспортировке питомца":
+                        getRecommendationsAnimalTransportation(chatId);
+                        understood = true;
+                        break;
+                    case "Обустройство дома для питомца с ограниченными возможностями":
+                        getRecommendationsHomeImprovementForDisabledPet(chatId);
+                        understood = true;
+                        break;
+                    case "Запросить связь":
+                        writeDownContactPhoneNumber(chatId);
+                        understood = true;
+                        break;
+                    default:
+                        sendMainMenu(chatId);
+                        break;
+                }
+
+                if (!understood) {
+                    var count = incorrectCounts.getOrDefault(chatId, 0);
+                    if (count < 1) {
+                        incorrectCounts.put(chatId, count + 1);
+                        writeIncorrectText(chatId, text);
+                    } else {
+                        writeIncorrectText2(chatId, text);
+                        incorrectCounts.remove(chatId);
+                    }
                 }
             }
         }
+    }
+
+        private void writeDownContactPhoneNumber(long chatId) {
+            String text = "Я могу записать Ваши контактные данные и в ближайшее время с Вами свяжется наш волонтер и проконсультируют Вас. " +
+                    "Введите номер телефона";
+            SendMessage message = new SendMessage(chatId, text);
+            telegramBot.execute(message);
+            userStates.put(chatId, "PhoneListener");
+        }
+
+        //Метод, определяющий правильность номера телефона и позволяющий записать контактные данные в БД при корректном их написании
+        private void handleContactInput(Long chatId, String text) {
+            Matcher matcher = PHONE_PATTERN.matcher(text);
+            if (matcher.matches()) {
+                var task = repository.findByChatId(chatId);
+                task.setPhone(text);
+                repository.save(task);
+                SendMessage message = new SendMessage(chatId, "Номер телефона успешно сохранен! Нажмите кнопку /menu");
+                telegramBot.execute(message);
+            } else {
+                SendMessage message = new SendMessage(chatId, "Неверный формат номера телефона. Пожалуйста, введите номер в формате:" + "+7-9**-**-**");
+                telegramBot.execute(message);
+            }
+        }
+
+    public void sendInstructionMenu(long chatId) {
+        telegramBot.execute(new SendMessage(chatId, "Выберите инструкцию:")
+                .replyMarkup(instructionMenuKeyboard));
+
     }
 
     private void writeIncorrectText(long chatId, String text) {
@@ -123,6 +190,20 @@ public class BotService {
                 new String[]{"Информация о приюте для кошек", "Информация о приюте для собак"},
                 new String[]{"Главное меню"}
         ));
+        telegramBot.execute(message);
+    }
+
+    private void getRecommendationsHomeImprovementForDisabledPet(long chatId) {
+        String text = "Рекомендации по обустройству дома для питомца с ограниченными возможностями Вы получите дополнительно по этой ссылке: \n" +
+                "https://petscage.ru/blog/kak-oblegchit-zhizn-zhivotnomu-invalidu/?srsltid=AfmBOorBT8vqYA28euOHitmhcZBTN-B3c8JjIqCxpK_5K7XBOM_UBjkC";
+        SendMessage message = new SendMessage(chatId, text);
+        telegramBot.execute(message);
+    }
+
+    private void getRecommendationsAnimalTransportation(long chatId) {
+        String text = "Рекомендации по транспортировке питомца Вы получите по этой ссылке: \n" +
+                "(https://www.purina.ru/find-a-pet/articles/getting-a-dog/adoption/adopting-a-dog)*.";
+        SendMessage message = new SendMessage(chatId, text);
         telegramBot.execute(message);
     }
 
@@ -155,7 +236,7 @@ public class BotService {
     }
 
     private void sendInstruction(long chatId) {
-        telegramBot.execute(new SendMessage(chatId, "Инструкция как взять животное из приюта: ..."));
+        telegramBot.execute(new SendMessage(chatId, "Инструкция как взять животное из приюта: ...").replyMarkup(instructionMenuKeyboard));;
     }
 
     private void sendPetReport(long chatId) {
---
+++ src/main/java/pro/sky/telegrambot/service/UserService.java
@@ -0,0 +1,19 @@
+package pro.sky.telegrambot.service;
+
+import org.springframework.stereotype.Service;
+import pro.sky.telegrambot.model.User;
+import pro.sky.telegrambot.repository.UserRepository;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User findByUser(long chatId) {
+        return userRepository.findByChatId(chatId);
+    }
+
+}
